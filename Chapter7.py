2183626ca0631816808b9999fd6cb2a26f0de73d
Sarah Tolley <stolley@ucla.edu>
2015-02-10 00:47:56 +0000

    second half of homework

--- Chapter7.py
+++ Chapter7.py
@@ -25,3 +25,30 @@ for acc in accs:
 for acc in accs:
 	if re.search(r"d.e", acc):
 		print("d and e in that order with letter: " + "\t" + acc)
+
+#contain both the letters d and e in any order
+for acc in accs:
+	if re.search(r"d.*e", acc) or re.search(r"e.*d", acc):
+		print("d and e in any order: " + "\t" + acc)
+
+#start with x or y
+for acc in accs:
+	if re.search(r"x|y", acc):
+		print("start with x or y: " + "\t" + acc)
+
+#start with x or y and end with e
+for acc in accs:
+	if re.search(r"^(x|y).*e$", acc):
+		print("start with x or y and end with e: " + "\t" + acc)
+
+#contain three or more numbers in a row
+for acc in accs:
+	if re.search(r"\d{3,}", acc):
+		print("three or more numbers in a row: " + "\t" + acc)
+
+#end with d followed by either a, r or p
+for acc in accs:
+	if re.search(r"d[arp]$", acc):
+		print("end with d followed by either a,r, or p: " + "\t" + acc)
+
+
